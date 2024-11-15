00f1d342c542443db0b50bc22dd52c0237853ac6
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-11-15 09:55:59 +0700

    Update Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,29 +17,30 @@ provider "azurerm" {
   features {}
 }
 
-#create service bus instance
+# Create service bus instance
 resource "azurerm_servicebus_namespace" "svc_bus" {
   name                			= "${var.service_bus_name}"
   location            			= "${var.location}"
   resource_group_name 			= "${var.resource_group_name}"
   sku                 			= "${var.service_bus_sku}"
   minimum_tls_version 			= 1.2
-  capacity 						= 1
-  premium_messaging_partitions 	= 1
+  capacity 						= 0
+  premium_messaging_partitions 	= 0
   local_auth_enabled			= true
   public_network_access_enabled = true
   tags     			  			= var.tags
 }
 
-#create queue
+# Create queue
 resource "azurerm_servicebus_queue" "svc_bus_queue" {
   name         = "${var.service_bus_queue_name}"
   namespace_id = azurerm_servicebus_namespace.svc_bus.id
   enable_partitioning = true
 }
 
+# Create rule
 resource "azurerm_servicebus_queue_authorization_rule" "queue_authorization_rule" {
-  name     = "queuerule"
+  name     = "queue-rule"
   queue_id = azurerm_servicebus_queue.svc_bus_queue.id
 
   listen = true
